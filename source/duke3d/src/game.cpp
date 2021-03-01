7f9fc954941a92e7de0a8b81c71fb414726f7064
Dino Bollinger <doom64hunter@gmail.com>
2021-03-01 11:46:57 +0100

    AMC: Disable the eduke32 update checks completely

--- source/duke3d/src/game.cpp
+++ source/duke3d/src/game.cpp
@@ -6487,6 +6487,7 @@ int app_main(int argc, char const* const* argv)
 #endif
     CONFIG_ReadSetup();
 
+#if 0
 #if defined(_WIN32) && !defined (EDUKE32_STANDALONE)
 
 //    initprintf("build %d\n",(uint8_t)Batoi(BUILDDATE));
@@ -6528,6 +6529,7 @@ int app_main(int argc, char const* const* argv)
             else initprintf("update: failed to check for updates\n");
         }
     }
+#endif
 #endif
 
 #ifdef EDUKE32_STANDALONE
